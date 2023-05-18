4fbea0f6c5cb92b3027435a81d3a7e9f9119291e
apani05 <apani@uw.edu>
2023-05-18 02:46:25 -0700

    more features added

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-const targetWord = "apple";
+let targetWord = generateRandomWord();
 let remainingGuesses = 5;
+let remainingTime = 180; // 3 minutes in seconds
+let countdownInterval;
+
+window.onload = function () {
+    disableActivity();
+};
+
+function disableActivity() {
+    const inputField = document.getElementById("inputField");
+    const checkButton = document.getElementById("wordle-btn");
+    checkButton.disabled = true;
+    inputField.disabled = true;
+    checkButton.classList.add("disabled");
+}
+
+function enableActivity() {
+    const inputField = document.getElementById("inputField");
+    const checkButton = document.getElementById("wordle-btn");
+    checkButton.disabled = false;
+    inputField.disabled = false;
+    checkButton.classList.remove("disabled");
+}
+
+function generateRandomWord() {
+    const wordList = [
+        "lucky", "table", "amber", "cloud", "dairy",
+        "frost", "knife", "roast", "tiger", "vital",
+        "yacht", "bluff", "crisp", "doubt", "eager",
+        "flame", "grace", "haste", "jelly", "knock",
+        "laugh", "mango", "noble", "oasis", "piano"
+    ];
+    return wordList[Math.floor(Math.random() * wordList.length)];
+}
 
 function checkGuess() {
     const inputField = document.getElementById("inputField");
     const guessWord = inputField.value.toLowerCase();
     const resultText = document.getElementById("result");
-    const checkButton = document.getElementById("wordle-btn");
 
     let hintText = "";
     let incorrectPositions = 0;
@@ -14,8 +46,8 @@ function checkGuess() {
     let unmatchedTarget = [];
 
     if (guessWord.length !== targetWord.length) {
-        resultText.textContent = "Incorrect guess. Guesses must be " + 
-        targetWord.length + " characters long. ";
+        resultText.textContent = "Incorrect guess. Guesses must be " +
+            targetWord.length + " characters long. ";
         resultText.style.color = "red";
         inputField.value = "";
         return;
@@ -39,7 +71,7 @@ function checkGuess() {
             hintText += '<span class="yellow-letter">' + guessWord[i] + '</span>';
             const charIndex = unmatchedGuess.indexOf(guessWord[i]);
             unmatchedGuess = unmatchedGuess.slice(0, charIndex).concat(unmatchedGuess.slice(charIndex + 1));
-            
+
             const wordIndex = unmatchedTarget.indexOf(guessWord[i]);
             unmatchedTarget = unmatchedTarget.slice(0, wordIndex).concat(unmatchedTarget.slice(wordIndex + 1));
         }
@@ -51,8 +83,9 @@ function checkGuess() {
     if (matchedPositions.length === targetWord.length) {
         resultText.innerHTML = "Congratulations! You guessed the word!";
         resultText.style.color = "green";
-        inputField.disabled = true;
-        checkButton.disabled = true;
+        disableActivity();
+        clearInterval(countdownInterval);
+        document.getElementById("timer").innerHTML = "03:00";
     } else if (remainingGuesses > 1) {
         remainingGuesses--;
         resultText.innerHTML =
@@ -64,12 +97,43 @@ function checkGuess() {
         inputField.value = "";
     } else {
         resultText.innerHTML =
-            "Out of guesses. <br>The target word was: " + 
+            "Out of guesses. <br>The target word was: " +
             targetWord + ".<br> Better luck next time!";
         resultText.style.color = "red";
-        inputField.disabled = true;
-        checkButton.disabled = true;
+        disableActivity();
+        clearInterval(countdownInterval);
+        document.getElementById("timer").innerHTML = "03:00";
     }
 
     inputField.value = "";
 }
+
+function startTimer() {
+    countdownInterval = setInterval(() => {
+        remainingTime--;
+
+        const minutes = Math.floor(remainingTime / 60).toString().padStart(2, "0");
+        const seconds = (remainingTime % 60).toString().padStart(2, "0");
+        const timerElement = document.getElementById("timer");
+        timerElement.innerHTML = minutes + ":" + seconds;
+
+        if (remainingTime === 0) {
+            clearInterval(countdownInterval);
+            timerElement.innerHTML = "Time's up!";
+            disableActivity();
+        }
+    }, 1000);
+
+}
+
+function startGame() {
+    targetWord = generateRandomWord();
+    remainingGuesses = 5;
+    remainingTime = 180;
+    clearInterval(countdownInterval);
+    document.getElementById("timer").innerHTML = "03:00";
+    startTimer();
+    enableActivity();
+    document.getElementById("result").innerHTML = "";
+}
+
